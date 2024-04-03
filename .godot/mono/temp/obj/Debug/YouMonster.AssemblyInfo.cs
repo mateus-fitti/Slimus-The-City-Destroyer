6eac486de648343122694114ff91d0a1418502d5
Mateus Fittipaldi <mateusfitti@gmail.com>
2024-04-03 18:58:29 -0300

    Update sound effects

--- .godot/mono/temp/obj/Debug/YouMonster.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/YouMonster.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YouMonster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5662f0713ff882b316267228f7a58fc68f6b116")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9a7eec0271b3913deee753ec880258e589d3ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("YouMonster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YouMonster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
